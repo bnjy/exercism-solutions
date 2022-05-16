cca0f396195b9c70839f09fc517f81d70af3ffc3
bnjy <benjaminroth@me.com>
2022-05-16 23:51:53 +0200

    initial commit

---
+++ java/log-levels/src/main/java/LogLevels.java
@@ -0,0 +1,24 @@
+public class LogLevels {
+
+    // main for local tests
+    public static void main(String[] args) {
+        LogLevels.reformat("[ERROR]: \t Corrupt disk\t \t \r\n");
+    }
+    
+    public static String message(String logLine) {        
+        String[] splitLogLine = logLine.split("]:");
+        return splitLogLine[1].trim();
+    }
+
+    public static String logLevel(String logLine) {        
+        String newLogLine = logLine.replace("[", "");
+        String[] splitLogLine = newLogLine.split("]:");
+        return splitLogLine[0].trim().toLowerCase();
+    }
+
+    public static String reformat(String logLine) {
+        String message = LogLevels.message(logLine);
+        String level = LogLevels.logLevel(logLine);
+        return message + " " + "(" + level + ")";
+    }
+}
