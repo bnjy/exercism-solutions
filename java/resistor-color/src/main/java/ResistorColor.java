f4cb46f8ce3566cd568c66efe62041f88392069e
bnjy <benjaminroth@me.com>
2022-05-19 22:55:28 +0200

    arrayes, easy peasy

---
+++ java/resistor-color/src/main/java/ResistorColor.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+class ResistorColor {
+
+    private static String[] colorArray = {
+        "black",
+        "brown",
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "violet",
+        "grey",
+        "white"
+    };
+
+    static int value;
+
+    public static void main(String[] args) {
+        System.out.println(colorCode("black"));    
+    }
+
+    static int colorCode(String color) {
+        for(int i = 0; i <= colorArray.length-1; i++){
+            if(color == colorArray[i]){
+                value = i;
+            }
+        }
+        return value;
+    }
+
+    static String[] colors() {
+        return colorArray;
+    }
+}
