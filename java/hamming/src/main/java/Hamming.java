4535173f0007cb04be73dc9f9eddb574a0ac08c0
bnjy <benjaminroth@me.com>
2022-10-16 19:53:56 +0200

    hamming distance

--- java/hamming/src/main/java/Hamming.java
+++ java/hamming/src/main/java/Hamming.java
@@ -5,15 +5,37 @@ public class Hamming {
 
   //class constructor for main class
   public Hamming(String leftStrand, String rightStrand) {
+    if(leftStrand.isEmpty() && !rightStrand.isEmpty()){
+      throw new IllegalArgumentException("left strand must not be empty.");
+    }
+
+    if(rightStrand.isEmpty() && !leftStrand.isEmpty()){
+      throw new IllegalArgumentException("right strand must not be empty.");
+    }
+
+    if(leftStrand.length() != rightStrand.length()){
+      throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+    }
+
     this.leftStrand = leftStrand;
     this.rightStrand = rightStrand;
   } 
   
   //main for local tests
   public static void main(String[] args) {
+    Hamming a1 = new Hamming("GAGCCTACTAACGGGAT", "CATCGTAATGACGGCCT");
+        
+    System.out.println("Hamming distance: " + a1.getHammingDistance());
   }
 
   public int getHammingDistance() {
-      throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+      int distance = 0; 
+
+      for(int i = 0; i < leftStrand.length(); i++){
+        if(this.leftStrand.charAt(i) != this.rightStrand.charAt(i)){
+          distance++;
+        }
+      }
+      return distance;
   }
 }
